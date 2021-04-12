387d654ba0c6fc96975e765a49b96a91fed1255b
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-12 19:04:13 +0200

    milestone 3 completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,26 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    searchMovie: '',
+    searchMovie: 'avengers',
     movies: [],
     tvShows: [],
   },
   methods: {
     search: function(){
+      let search = this.searchMovie;
+      this.searchMovie = '';
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=07d44c193ccfbcc66cd9b564a4d50433&language=it&query=${this.searchMovie}`)
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=07d44c193ccfbcc66cd9b564a4d50433&language=it&query=${search}`)
       .then((response) => {
         const result = response.data.results;
         this.movies = result;
-        console.log(this.movies);
       });
       axios
-      .get(`https://api.themoviedb.org/3/search/tv?api_key=07d44c193ccfbcc66cd9b564a4d50433&language=it&query=${this.searchMovie}`)
+      .get(`https://api.themoviedb.org/3/search/tv?api_key=07d44c193ccfbcc66cd9b564a4d50433&language=it&query=${search}`)
       .then((response) => {
         const result = response.data.results;
         this.tvShows = result;
-        console.log(this.tvShows);
+
       });
 
     },
