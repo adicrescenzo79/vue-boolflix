3edf83d027537064544c2fc9026465ad6ce75908
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-12 15:25:08 +0200

    milestone 1 completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,29 @@
-// SCOMMENTARE SE SI USA JQUERY
-// $(document).ready(function(){
-//
-// });
-
-// METODO JQUERY PER LA RICHIESTA DI API
-$.ajax(
-{
-url: "https://flynn.boolean.careers/exercises/api/random/boolean",
-method: "GET",
-success: function (data, stato) {
-$("#risultati").html(data);
-},
-error: function (richiesta, stato, errori) {
-alert("E' avvenuto un errore. " + errore);
-}
-}
-);
-
-// SCOMMENTARE SE SI USA VUE
 Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Vue!'
+    searchMovie: '',
+    movies: [],
+  },
+  methods: {
+    search: function(){
+      axios
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=07d44c193ccfbcc66cd9b564a4d50433&language=it&query=${this.searchMovie}`)
+      .then((response) => {
+        const result = response.data.results;
+        this.movies = result;
+        console.log(this.movies);
+      });
+    },
   }
 })
 
 // METODO VUE PER LA RICHIESTA DI API
-axios
-.get('https://flynn.boolean.careers/exercises/api/random/boolean')
-.then(function (response) {
-  const result = response.data;
-})
-.catch((error) => {
-  console.log(error);
-});
+// axios
+// .get('https://flynn.boolean.careers/exercises/api/random/boolean')
+// .then(function (response) {
+//   const result = response.data;
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
